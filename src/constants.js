8bec7abeb95fdd9c60f7336664e0304b1088568e
Joris <joris.coppieters@gmail.com>
2017-08-04 10:36:04 +1200

    Set version to 0.4.4

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@
 // ******************************
 
 const c_SCRIPT_NAME = 'Servicerator';
-const c_VERSION = '0.4.3';
+const c_VERSION = '0.4.4';
 
 const c_MODE_HELP = '[HELP]';
 const c_MODE_VERSION = '[VERSION]';
