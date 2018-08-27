a99ebc5c51c931ddaec6d83103e006eb4b3978ed
Joris <joris.coppieters@gmail.com>
2018-08-27 12:42:49 +1200

    Set version to 0.16.3

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@
 // ******************************
 
 const c_SCRIPT_NAME = 'Servicerator';
-const c_VERSION = '0.16.2';
+const c_VERSION = '0.16.3';
 
 const c_MODE_HELP = '[HELP]';
 const c_MODE_VERSION = '[VERSION]';
