c844c5612c19259daa8149753a371501a2a2c8ed
Joris Coppieters <joris@trademe.co.nz>
2019-03-28 13:59:05 +1300

    Fixes to password prompt

--- src/utils/readline.js
+++ src/utils/readline.js
@@ -13,8 +13,8 @@ const print = require('./print');
 // Functions:
 // ******************************
 
-function readLineSync (in_question) {
-    print.out(cprint.toMagenta(in_question));
+function _readLineSync (in_question) {
+    print.out(in_question);
     let input = '';
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
@@ -23,24 +23,69 @@ function readLineSync (in_question) {
     });
     while(!input) {require('deasync').sleep(100);}
     process.stdin.pause();
-    if (env.isMinGW()) {
-        print.out('\n');
-    }
     return input.trim();
 }
 
 // ******************************
 
-function readHiddenLineSync (in_question) {
-    if (!env.isMinGW()) {
-        return readLineSync(in_question);
+function readLineSync (in_question) {
+    return _readLineSync(cprint.toMagenta(in_question + ':') + ' ');
+}
+
+// ******************************
+
+function readHiddenLineSync (in_question, in_username) {
+    if (env.isMinGW()) {
+        return _readHiddenLineSyncMinGW(in_question);
     }
 
+    if (env.isWindows()) {
+        try {
+            return _readHiddenLineSyncWindows(in_question, in_username);
+        } catch (e) {
+            return _readHiddenLineSyncInClear(in_question);
+        }
+    }
+
+    return _readHiddenLineSyncInClear(in_question);
+}
+
+// ******************************
+
+function _readHiddenLineSyncMinGW (in_question) {
     const readlineSync = require('readline-sync');
-    const input = readlineSync.question(cprint.toMagenta(in_question), {
+    const input = readlineSync.question(cprint.toMagenta(in_question + ': '), {
         hideEchoBack: true
     });
     return input;
+}
+
+// ******************************
+
+function _readHiddenLineSyncWindows (in_question, in_username) {
+    const exec = require('./exec');
+    const fs = require('./filesystem');
+    const path = require('path');
+
+    print.out(cprint.toMagenta(in_question) + ' ' + cprint.toYellow('(in Windows Credential Window)') + '\n');
+
+    const bundledScriptPath = path.join(__dirname, 'readPassword.ps1');
+    const copiedScriptPath = path.join(env.getTemp(), 'svr-read-password.ps1');
+    fs.writeFile(copiedScriptPath, fs.readFile(bundledScriptPath));
+    const cmdResult = exec.cmdSync('powershell', ['-f', copiedScriptPath, '-prompt', in_username], {
+        hide: true
+    });
+    fs.deleteFile(copiedScriptPath);
+    if (cmdResult.hasError) {
+        throw cmdResult.error;
+    }
+    return cmdResult.result;
+}
+
+// ******************************
+
+function _readHiddenLineSyncInClear (in_question) {
+    return _readLineSync(cprint.toMagenta(in_question) + cprint.toYellow(' (due to shell incompatibility, the input cannot be hidden): '));
 }
 
 // ******************************
