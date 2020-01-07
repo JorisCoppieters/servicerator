935c4a33edb50de461b38ea406cc717a5f9eb081
Joris Coppieters <joris@trademe.co.nz>
2020-01-08 12:58:49 +1300

    Set version to 0.19.16

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@
 // ******************************
 
 const c_SCRIPT_NAME = 'Servicerator';
-const c_VERSION = '0.19.15';
+const c_VERSION = '0.19.16';
 
 const c_MODE_HELP = '[HELP]';
 const c_MODE_VERSION = '[VERSION]';
